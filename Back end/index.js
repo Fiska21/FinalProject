b72014337a2f6a0e5ddedf81facdef697078b770
unknown <fiskahendiyaningsih@gmail.com>
2022-03-27 12:20:02 +0700

    backend

---
+++ Back end/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const authRoute = require("./routes/auth")
+const userRoute = require("./routes/users")
+const movieRoute = require("./routes/movies")
+const listRoute = require("./routes/lists")
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+    // useCreateIndex : true,
+})
+.then(() => console.log("DB connection successfull"))
+.catch(() => console.log(error));
+
+app.use(express.json())
+
+app.use("/api/auth",authRoute)
+app.use("/api/users", userRoute)
+app.use("/api/movies", movieRoute)
+app.use("/api/lists", listRoute)
+
+// main().catch(err => console.log(err));
+
+// async function main() {
+//   await mongoose.connect('mongodb://localhost:27017/test');
+// }
+
+app.listen(8800, () => {
+    console.log("Backend server is running");
+})
